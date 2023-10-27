541f0e4d97c766312b0ffacf10fa87bb3c7f1e50
Mund3k <74401169+kubat1611@users.noreply.github.com>
2023-10-27 09:26:09 +0200

    1
    male zmiany do 1 zadanka

--- src/z1/Pracownik.java
+++ src/z1/Pracownik.java
@@ -1,19 +1,24 @@
 package z1;
 
-public class Pracownik {
-    private final Pracownik przelozony;
-    private final int pensja;
+public final class Pracownik {
+    private Pracownik przelozony;
+    private int pensja;
 
-    public Pracownik(Pracownik przelozony, int pensja) {
+    public Pracownik(Pracownik przelozony){
+        this.przelozony = przelozony;
+        this.pensja = 3600;
+    }
+
+    public Pracownik(Pracownik przelozony, int pensja){
         this.przelozony = przelozony;
         this.pensja = pensja;
     }
 
-    public int getPensja() {
-        return pensja;
+    public int getPensja(){
+        return this.pensja;
     }
 
-    public Pracownik getPrzelozony() {
-        return przelozony;
+    public Pracownik getPrzelozony(){
+        return this.przelozony;
     }
 }
